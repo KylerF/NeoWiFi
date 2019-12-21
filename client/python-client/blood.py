dc8613bdcdfdee23989a2c38a4599035d37145ab
Kyler Freas <kmf5285@rit.edu>
2019-12-21 11:19:19 -0500

    error handling

--- client/python-client/blood.py
+++ client/python-client/blood.py
@@ -2,8 +2,10 @@
 import requests
 import random
 import time
+import sys
 
 esp_url = 'http://192.168.1.27/write'
+
 num_leds = 29
 i = 0
 
@@ -17,13 +19,18 @@ while 1:
     for x in range(num_leds):
         if x == i:
             r = brightness
-             
+            
             payload.append({"index": x, "r": r, "g": 0, "b": 0, "w": 0})
         else:
             payload.append({"index": x, "r": 0, "g": 0, "b": 0, "w": 0})
-     
-    r = requests.post(esp_url, json=payload)
-    i = (i + 1) % num_leds
-
-    time.sleep(speed)
+    
+    try:
+        r = requests.post(esp_url, json=payload, timeout=1)
+        i = (i + 1) % num_leds
+        time.sleep(speed)
+    except requests.exceptions.ConnectionError as e:
+        print("Error Connecting: ", e)
+        
+        if 'Max retries' in str(e):
+            sys.exit(1)
 
