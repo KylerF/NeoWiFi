aea21733e2874787e7be3bb23b1704f57e66dc3d
Kyler Freas <kmf5285@rit.edu>
2019-12-23 09:51:05 -0500

    fixed the offset thing:wq

--- client/python-client/blood.py
+++ client/python-client/blood.py
@@ -4,33 +4,46 @@ import random
 import time
 import sys
 
-esp_url = 'http://192.168.1.27/write'
+esp_url = 'http://192.168.1.3/write'
 
-num_leds = 29
-i = 0
+num_leds1 = 144
+num_leds2 = 103
+
+i = num_leds1
+j = 0
 
 while 1:
     payload = []
 
-    if i == 0:
-        brightness = random.randint(0, 255)
+    if i == num_leds1:
+        brightness = 255
         speed = brightness / 10000 
  
-    for x in range(num_leds):
+    for x in range(num_leds1):
         if x == i:
             r = brightness
             
-            payload.append({"index": x, "r": r, "g": 0, "b": 0, "w": 0})
-        else:
-            payload.append({"index": x, "r": 0, "g": 0, "b": 0, "w": 0})
-    
+            payload.append({"index": x, "r": r, "g": 0, "b": 0})
+        elif x == i + 1:
+            payload.append({"index": x, "r": 5, "g": 0, "b": 0})
+            
+    for x in range(num_leds2):
+        if x == j:
+            r = brightness
+            payload.append({"index": x + num_leds1, "r": r, "g": 0, "b": 0})
+        elif x == j - 1:
+            payload.append({"index": x + num_leds1, "r": 5, "g": 0, "b": 0})
+
     try:
         r = requests.post(esp_url, json=payload, timeout=1)
-        i = (i + 1) % num_leds
+        i = (i - 1) % num_leds1
+        j = (j + 1) % num_leds2
         time.sleep(speed)
+    
     except requests.exceptions.ConnectionError as e:
         print("Error Connecting: ", e)
         
-        if 'Max retries' in str(e):
+        if "Max retries" in str(e):
             sys.exit(1)
+
 
